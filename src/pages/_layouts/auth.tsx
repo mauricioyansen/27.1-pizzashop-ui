31c364104ce0620b06ab13b5897aece636310acb
Mauricio <mauricio_cy@hotmail.com>
2025-03-19 13:40:51 -0300

    login page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,20 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="size-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
