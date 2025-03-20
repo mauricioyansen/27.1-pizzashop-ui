078c014c0addf80cdff4764d59c4a4dbd37a09ff
Mauricio <mauricio_cy@hotmail.com>
2025-03-20 10:23:10 -0300

    axios, react-query and .env setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { querClient } from "./lib/react-query";
 
 export function App() {
   return (
     <>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={querClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
