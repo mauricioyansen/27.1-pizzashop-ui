03f1f424ce5cf6d825d388aa8467e333f071c83a
Mauricio <mauricio_cy@hotmail.com>
2025-03-21 10:54:26 -0300

    removing react day picker

--- src/api/get-daily-revenue-in-period.ts
+++ src/api/get-daily-revenue-in-period.ts
@@ -1,24 +1,24 @@
 import { api } from "@/lib/axios";
 
-export interface GetDailyRevenueInPeriodQuery {
-  from?: Date;
-  to?: Date;
-}
+// export interface GetDailyRevenueInPeriodQuery {
+//   from?: Date;
+//   to?: Date;
+// }
 
 export type GetDailyRevenueInPeriodResponse = {
   date: string;
   receipt: number;
 }[];
 
-export async function getDailyRevenueInPeriod({
+export async function getDailyRevenueInPeriod(/*{
   from,
   to,
-}: GetDailyRevenueInPeriodQuery) {
+}: GetDailyRevenueInPeriodQuery*/) {
   const res = await api.get<GetDailyRevenueInPeriodResponse>(
-    "/metrics/daily-receipt-in-period",
-    {
+    "/metrics/daily-receipt-in-period"
+    /*{
       params: { from, to },
-    }
+    }*/
   );
 
   return res.data;
--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -6,11 +6,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { DateRangePicker } from "@/components/ui/date-range-picker";
-import { Label } from "@/components/ui/label";
+// import { DateRangePicker } from "@/components/ui/date-range-picker";
+// import { Label } from "@/components/ui/label";
 import { useQuery } from "@tanstack/react-query";
-import { useMemo, useState } from "react";
-import { DateRange } from "react-day-picker";
+// import { useMemo, useState } from "react";
+// import { DateRange } from "react-day-picker";
 import {
   ResponsiveContainer,
   LineChart,
@@ -19,32 +19,33 @@ import {
   CartesianGrid,
   Line,
 } from "recharts";
-import { subDays } from "date-fns";
+// import { subDays } from "date-fns";
 import { Loader2 } from "lucide-react";
 
 export function RevenueChart() {
-  const [dateRange, setDateRange] = useState<DateRange | undefined>({
-    from: subDays(new Date(), 7),
-    to: new Date(),
-  });
+  // const [dateRange, setDateRange] = useState<DateRange | undefined>({
+  //   from: subDays(new Date(), 7),
+  //   to: new Date(),
+  // });
 
   const { data: dailyRevenueInPeriod } = useQuery({
-    queryKey: ["metrics", "daily-revenue-in-period-products", dateRange],
-    queryFn: () =>
-      getDailyRevenueInPeriod({
-        from: dateRange?.from,
-        to: dateRange?.to,
-      }),
+    queryKey: ["metrics", "daily-revenue-in-period-products", /*dateRange*/],
+    queryFn: getDailyRevenueInPeriod
+    // queryFn: () =>
+    //   getDailyRevenueInPeriod({
+    //     from: dateRange?.from,
+    //     to: dateRange?.to,
+    //   }),
   });
 
-  const chartData = useMemo(() => {
-    return dailyRevenueInPeriod?.map((chartItem) => {
-      return {
-        date: chartItem.date,
-        receipt: chartItem.receipt / 100,
-      };
-    });
-  }, [dailyRevenueInPeriod]);
+  // const chartData = useMemo(() => {
+  //   return dailyRevenueInPeriod?.map((chartItem) => {
+  //     return {
+  //       date: chartItem.date,
+  //       receipt: chartItem.receipt / 100,
+  //     };
+  //   });
+  // }, [dailyRevenueInPeriod]);
 
   return (
     <Card className="col-span-6">
@@ -56,15 +57,15 @@ export function RevenueChart() {
           <CardDescription>Receita diária no período</CardDescription>
         </div>
 
-        <div className="flex items-center gap-3">
+        {/* <div className="flex items-center gap-3">
           <Label>Período</Label>
           <DateRangePicker date={dateRange} onDateChange={setDateRange} />
-        </div>
+        </div> */}
       </CardHeader>
       <CardContent>
-        {chartData ? (
+        {dailyRevenueInPeriod ? (
           <ResponsiveContainer width="100%" height={240}>
-            <LineChart data={chartData} style={{ fontSize: 12 }}>
+            <LineChart data={dailyRevenueInPeriod} style={{ fontSize: 12 }}>
               <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
               <YAxis
                 stroke="#888"
